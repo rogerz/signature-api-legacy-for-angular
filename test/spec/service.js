010ad998091b53e2c5dc14e607ab5818f3596826
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-11-26 23:02:35 +0800

    Add test cases

--- test/spec/service.js
+++ test/spec/service.js
@@ -1,24 +1,63 @@
 describe('signature API', function () {
   var signatureApi;
+  var hosts = {
+    'test': 'http://10.0.0.1',
+    'dev': 'http://10.0.0.2',
+    'prod': 'http://10.0.0.3'
+  };
+  var response = {
+    list: [{
+      "id":"1241",
+      "orig_filename":"signatures/1/1.png",
+      "trans_filename":"signatures/1/1.png",
+      "signature":"2013-11-09 07:23:56",
+      "directory":"1"
+    },{
+      "id":"1242",
+      "orig_filename":"signatures/1/2.png",
+      "trans_filename":"signatures/1/2.png",
+      "signature":"2013-11-09 07:25:28",
+      "directory":"1"
+    }],
+    update: []
+  };
+
+  var $httpBackend;
+
   beforeEach(function () {
     module(require('signature-api-legacy-for-angular').name);
-    inject(function (_signatureApi_) {
-      signatureApi = _signatureApi_;
+    module(function(signatureApiProvider) {
+      expect(angular.isFunction(signatureApiProvider.hosts)).toBe(true);
+      signatureApiProvider.hosts(hosts);
     });
+    inject(function ($injector) {
+      $httpBackend = $injector.get('$httpBackend');
+      signatureApi = $injector.get('signatureApi');
+    });
+    signatureApi.setup('test', {eventId: 118});
   });
 
-  it('should before each', function () {
-    expect(true).toBe(true);
-  });
-/*
-  it('should allow initing hosts', function () {
-    inject(function (signatureApiProvider) {
-      expect(angular.isFunction(signatureApiProvider.hosts)).toBe(true);
-    });
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   });
 
   it('should export API', function () {
-    expect(angular.isFunction(signatureApi.setup)).toBe(true);
+    ['setup', 'list', 'update', 'add'].forEach(function (api) {
+      expect(angular.isFunction(signatureApi[api])).toBe(true);
+    });
   });
-*/
+
+  it('should send request', function () {
+    $httpBackend.expectGET('http://10.0.0.1/?m=api&a=do_e_signature_list&c_event_id=118&directory=1')
+    .respond(200, response.list);
+    $httpBackend.expectGET('http://10.0.0.1/?last_id=1242&m=api&a=do_e_signature_list&c_event_id=118&directory=1')
+    .respond(200, response.update);
+
+    signatureApi.list(function () {
+      signatureApi.update();
+    });
+
+    $httpBackend.flush();
+  });
 });
