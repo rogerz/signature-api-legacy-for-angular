260c8e035c7f1071d0fb661b046b158b5f79fcb7
Rogerz Zhang <rogerz.zhang@gmail.com>
2013-11-26 19:15:14 +0800

    Use provider instead of service

--- index.js
+++ index.js
@@ -3,134 +3,136 @@ var query = require('querystring');
 var format = require('format');
 
 exports = module.exports =
-angular.module('letsface/signatureApiLegacy', [])
-.service('signatureApi', function ($http) {
-  // jshint camelcase: false
-  // letsface servers from https://github.com/letsface/signature-cluster/wiki/API
-  var hosts = {
-    // 'alias': 'http://xxx.xxx.xxx.xxx'
-  };
+  angular.module('letsface/signatureApiLegacy', [])
+  .provider('signatureApi', function ($http) {
+    // jshint camelcase: false
 
-  function init(opts) {
-    _.extend(hosts, opts.hosts);
-  }
-
-  var defaultOpts = {
-    m: 'api',
-    a: 'do_e_signature_list',
-    c_event_id: 118,
-    directory: 1
-  };
-
-  var stat = {};// service stat such as last_id;
-  var server;
-
-  /*
-   * Setup default option and clear status
-   *
-   * @param {String} host alias of host ('CN Test') or host address ('http://xxx.xxx.xxx.xxx')
-   * @param {Object} opts new options
-   */
-  function setup(host, opts) {
-    var d3ToLegacy = {
-      eventId: 'c_event_id'
+    // letsface servers from https://github.com/letsface/signature-cluster/wiki/API
+    var hosts = {
+      // 'alias': 'http://xxx.xxx.xxx.xxx'
     };
 
-    for (var d3Key in d3ToLegacy) {
-      var legacyKey = d3ToLegacy[d3Key];
-      opts[legacyKey] = opts[d3Key];
-      delete opts[d3Key];
-    }
+    this.hosts = function (h) {
+      _.extend(hosts, h);
+    };
 
-    server = hosts[host] || host;
-    _.extend(defaultOpts, opts);
-    stat = {};
-  }
+    this.$get = function () {
+      var defaultOpts = {
+        m: 'api',
+        a: 'do_e_signature_list',
+        c_event_id: 118,
+        directory: 1
+      };
 
-  /*
-   * List signatures
-   *
-   * @param {String} host host name
-   * @param {Object} opts override default options
-   * @param {Function} success callback when success
-   * @param {Function} error callback when error
-   *
-   * Example signatureService.list(successFn, errorFn);
-   */
-  function list(host, opts, success, error) {
+      var stat = {};// service stat such as last_id;
+      var server;
 
-    if (typeof host === 'object') {
-      error = success;
-      success = opts;
-      opts = host;
-    } else if (typeof host === 'function') {
-      error = opts;
-      success = host;
-    }
-
-    if (!opts) {
-      opts = {};
-    }
-
-    _.defaults(opts, defaultOpts);
-
-    var url = format('%s/?%s', server, query.stringify(opts));
-
-    return $http.get(url)
-    .error(error)
-    .success(function (data) {
-/*
- *  data format from https://github.com/letsface/signature-cluster/wiki/API
- *
-      [{
-        "id":"1241",
-        "orig_filename":"signatures/1/1.png",
-        "trans_filename":"signatures/1/1.png",
-        "signature":"2013-11-09 07:23:56",
-        "directory":"1"
-       },{
-        "id":"1242",
-        "orig_filename":"signatures/1/2.png",
-        "trans_filename":"signatures/1/2.png",
-        "signature":"2013-11-09 07:25:28",
-        "directory":"1"
-       }]
-
-   access url
-
-   http://backend.addr/hphoto/signatures/1/1.png
-*/
-      // format result for signature cloud
-
-      data = data.map(function (d) {
-        return {
-          id: d.id,
-          href: format('%s/hphoto/%s', server, d.trans_filename),
-          timestamp: d.signature
+      /*
+       * Setup default option and clear status
+       *
+       * @param {String} host alias of host ('CN Test') or host address ('http://xxx.xxx.xxx.xxx')
+       * @param {Object} opts new options
+       */
+      function setup(host, opts) {
+        var d3ToLegacy = {
+          eventId: 'c_event_id'
         };
-      });
-      if (data.length) {
-        stat.lastId = data[data.length - 1].id;
+
+        for (var d3Key in d3ToLegacy) {
+          var legacyKey = d3ToLegacy[d3Key];
+          opts[legacyKey] = opts[d3Key];
+          delete opts[d3Key];
+        }
+
+        server = hosts[host] || host;
+        _.extend(defaultOpts, opts);
+        stat = {};
       }
-      success(data);
-    });
-  }
 
-  /* submit signature to backend */
-  function add() {
-    console.log("signature added to backend");
-  }
+      /*
+       * List signatures
+       *
+       * @param {String} host host name
+       * @param {Object} opts override default options
+       * @param {Function} success callback when success
+       * @param {Function} error callback when error
+       *
+       * Example signatureService.list(successFn, errorFn);
+       */
+      function list(host, opts, success, error) {
 
-  /* get update since last query */
-  function update(success, error) {
-    return list({last_id: stat.lastId}, success, error);
-  }
+        if (typeof host === 'object') {
+          error = success;
+          success = opts;
+          opts = host;
+        } else if (typeof host === 'function') {
+          error = opts;
+          success = host;
+        }
 
-  return {
-    init: init,
-    setup: setup,
-    list: list,
-    update: update,
-    add: add
-  };
-});
+        if (!opts) {
+          opts = {};
+        }
+
+        _.defaults(opts, defaultOpts);
+
+        var url = format('%s/?%s', server, query.stringify(opts));
+
+        return $http.get(url)
+               .error(error)
+               .success(function (data) {
+                 /*
+                  *  data format from https://github.com/letsface/signature-cluster/wiki/API
+                  *
+[{
+"id":"1241",
+"orig_filename":"signatures/1/1.png",
+"trans_filename":"signatures/1/1.png",
+"signature":"2013-11-09 07:23:56",
+"directory":"1"
+},{
+"id":"1242",
+"orig_filename":"signatures/1/2.png",
+"trans_filename":"signatures/1/2.png",
+"signature":"2013-11-09 07:25:28",
+"directory":"1"
+}]
+
+access url
+
+http://backend.addr/hphoto/signatures/1/1.png
+                  */
+                 // format result for signature cloud
+
+                 data = data.map(function (d) {
+                   return {
+                     id: d.id,
+                     href: format('%s/hphoto/%s', server, d.trans_filename),
+                     timestamp: d.signature
+                   };
+                 });
+                 if (data.length) {
+                   stat.lastId = data[data.length - 1].id;
+                 }
+                 success(data);
+               });
+      }
+
+      /* submit signature to backend */
+      function add() {
+        console.log("signature added to backend");
+      }
+
+      /* get update since last query */
+      function update(success, error) {
+        return list({last_id: stat.lastId}, success, error);
+      }
+
+      return {
+        setup: setup,
+        list: list,
+        update: update,
+        add: add
+      };
+    };
+  });
